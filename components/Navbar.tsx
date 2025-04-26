63bc53c4a9556dd876046efc0978fda8049f9e25
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2025-04-26 16:13:00 +0200

    add navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="h-[10vh] bg-slate-100">
+      <div className="flex justify-between  px-16 py-4">
+        <div>
+          <h2>Ricky-Morty</h2>
+        </div>
+        <div className="flex justify-center items-center gap-4">
+          <div>
+            <Link href="/">Home</Link>
+          </div>
+          <div>
+            <Link href="/">Favourite</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
