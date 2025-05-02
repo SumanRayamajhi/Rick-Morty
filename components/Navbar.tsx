df20b4ffe0977acbe0ec47fcc33912a88de1c135
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2025-05-02 09:10:34 +0200

    add mobile menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,16 +2,21 @@
 
 import { useFavorites } from "@/app/context/FavoritesContext";
 import Link from "next/link";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
 
 function Navbar() {
   const { favorites } = useFavorites();
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className="h-[10vh] bg-slate-100 fixed top-0 w-full z-50">
+    <div className="h-[10vh] bg-slate-100 fixed top-0 w-full z-50 shadow-sm">
       <div className="flex justify-between items-center px-16 h-full">
         <Link href="/" className="cursor-pointer">
           <h2 className="font-bold text-lg">Rick-Morty</h2>
         </Link>
-        <div className="flex justify-center items-center gap-4">
+
+        <div className="hidden md:flex items-center gap-6">
           <div className="flex items-center gap-4">
             <Link href="/" className="font-semibold">
               Home
@@ -28,6 +33,31 @@ function Navbar() {
             </Link>
           </div>
         </div>
+
+        <button
+          className="flex md:hidden text-black cursor-pointer"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          {menuOpen ? <X size={24} /> : <Menu size={23} />}
+          {menuOpen && (
+            <div className="md:hidden px-6 pb-4 flex flex-col gap-4">
+              <Link href="/" className="font-semibold hover:text-blue-500">
+                Home
+              </Link>
+              <Link
+                href="/favorite"
+                className="relative font-semibold hover:text-blue-500"
+              >
+                {Object.keys(favorites).length > 0 && (
+                  <span className="absolute -top-1 -right-4 bg-white text-red-600 text-xs font-bold px-1.5 py-0.5 rounded-full">
+                    {Object.keys(favorites).length}
+                  </span>
+                )}
+                Favorite
+              </Link>
+            </div>
+          )}
+        </button>
       </div>
     </div>
   );
